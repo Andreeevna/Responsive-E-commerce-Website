f4ddff4cc1859882f811d2551d1ac1410d1b879b
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-23 14:18:12 +0300

    feat: created markup for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,30 @@
+import { RiShoppingCartLine } from 'react-icons/ri'
+
+import LogoImg from '../../assets/logo.png'
+import styles from './Header.module.css'
+
 const Header = () => {
-	return <div></div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.logo}>
+				<img src={LogoImg} alt='Logo' />
+				<span>Amazon</span>
+			</div>
+			<div className={styles.right}>
+				<div className={styles.menu}>
+					<ul className={styles.menu__list}>
+						<li>Collections</li>
+						<li>Brands</li>
+						<li>New</li>
+						<li>Sales</li>
+						<li>ENG</li>
+					</ul>
+				</div>
+				<input type='text' className={styles.search} />
+				<RiShoppingCartLine className={styles.cart} />
+			</div>
+		</div>
+	)
 }
 
 export default Header
