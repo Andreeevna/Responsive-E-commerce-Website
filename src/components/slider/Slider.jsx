c9aa1d5a7da08be47c0088dc72ff0911a2aa69fe
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-23 18:00:32 +0300

    fix: fixed a bug with modular styles

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -6,11 +6,11 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 import { SliderProducts } from '../../data/products'
-import styles from './Slider.module.css'
+import './Slider.css'
 
 const Slider = () => {
 	return (
-		<div className={styles.s_container}>
+		<div className='s_container'>
 			<Swiper
 				slidesPerView={3}
 				spaceBetween={40}
@@ -20,8 +20,8 @@ const Slider = () => {
 				{SliderProducts.map((product, index) => {
 					return (
 						<SwiperSlide key={product.name + index}>
-							<div className={styles.slide}>
-								<div className={styles.name}>
+							<div className='slide'>
+								<div className='name'>
 									<span>{product.name}</span>
 									<span>{product.detail}</span>
 								</div>
@@ -29,11 +29,7 @@ const Slider = () => {
 								<span>{product.price} $</span>
 								<button>Shop now</button>
 							</div>
-							<img
-								src={product.img}
-								alt='Product'
-								className={styles.img_product}
-							/>
+							<img src={product.img} alt='Product' className='img_product' />
 						</SwiperSlide>
 					)
 				})}
