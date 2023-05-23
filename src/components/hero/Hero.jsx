cf57c587dad3155279499418984f253dc9ba24c5
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-23 17:13:21 +0300

    style: stylized the hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BsArrowRightCircle } from 'react-icons/bs'
+import { BsArrowRight } from 'react-icons/bs'
 import { RiShoppingBagFill } from 'react-icons/ri'
 
 import HeroImg from '../../assets/hero.png'
@@ -10,41 +10,40 @@ const Hero = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.h_side}>
-				<span>SKIN PROTECTION CREAM</span>
-				<div className={styles.text}>
+				<span className={styles.text1}>SKIN PROTECTION CREAM</span>
+				<div className={styles.text2}>
 					<span>Trendy Collection</span>
 					<span>
 						Seedily say has suitable disposal and boy. Exercise joy man children
 						rejoiced.
 					</span>
 				</div>
-
-				{/* {m} */}
-				<div className={styles.mid_side}>
-					<div className={styles.circle}></div>
-					<img src={HeroImg} alt='' width={600} />
-					<div className={styles.cart}>
-						<RiShoppingBagFill />
-						<div className={styles.signup}>
-							<span>Best Signup Offers</span>
-							<div>
-								<BsArrowRightCircle />
-							</div>
+			</div>
+			{/* {m} */}
+			<div className={styles.mid_side}>
+				<div className={styles.circle}></div>
+				<img src={HeroImg} alt='' width={600} />
+				<div className={styles.cart}>
+					<RiShoppingBagFill />
+					<div className={styles.signup}>
+						<span>Best Signup Offers</span>
+						<div className={styles.signup_arrow}>
+							<BsArrowRight />
 						</div>
 					</div>
 				</div>
+			</div>
 
-				{/* {r} */}
+			{/* {r} */}
 
-				<div className={styles.h_side}>
-					<div className={styles.details}>
-						<span>1.5m</span>
-						<span>Monthly traffic</span>
-					</div>
-					<div className={styles.details}>
-						<span>100k</span>
-						<span>Happy Customers</span>
-					</div>
+			<div className={styles.h_side}>
+				<div className={styles.details}>
+					<span>1.5m</span>
+					<span>Monthly traffic</span>
+				</div>
+				<div className={styles.details}>
+					<span>100k</span>
+					<span>Happy Customers</span>
 				</div>
 			</div>
 		</div>
