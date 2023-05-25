8f7f67b95a5a653324dc217cc747c8bab41e84db
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-25 15:58:09 +0300

    feat: added animation for the central block in the Hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 import { BsArrowRight } from 'react-icons/bs'
 import { RiShoppingBagFill } from 'react-icons/ri'
 
+import { motion } from 'framer-motion'
+
 import HeroImg from '../../assets/hero.png'
 import styles from './Hero.module.css'
 
 const Hero = () => {
+	const transition = { duration: 3, type: 'spring' }
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.h_side}>
@@ -21,9 +25,26 @@ const Hero = () => {
 			</div>
 			{/* {m} */}
 			<div className={styles.mid_side}>
-				<div className={styles.circle}></div>
-				<img src={HeroImg} alt='' width={600} />
-				<div className={styles.cart}>
+				<motion.div
+					initial={{ bottom: '2rem' }}
+					whileInView={{ bottom: '0rem' }}
+					transition={transition}
+					className={styles.circle}
+				></motion.div>
+				<motion.img
+					initial={{ bottom: '2rem' }}
+					whileInView={{ bottom: '0rem' }}
+					transition={transition}
+					src={HeroImg}
+					alt=''
+					width={600}
+				/>
+				<motion.div
+					initial={{ right: '4%' }}
+					whileInView={{ right: '2%' }}
+					transition={transition}
+					className={styles.cart}
+				>
 					<RiShoppingBagFill />
 					<div className={styles.signup}>
 						<span>Best Signup Offers</span>
@@ -31,7 +52,7 @@ const Hero = () => {
 							<BsArrowRight />
 						</div>
 					</div>
-				</div>
+				</motion.div>
 			</div>
 
 			{/* {r} */}
