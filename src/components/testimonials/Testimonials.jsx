1e10b99ea303d90a8f154164cd5f6a483211407b
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-05-25 12:44:50 +0300

    feat: added markup to the header of the Testimonials component

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Hero } from '../../assets/hero.png'
+import styles from './Testimonials.module.css'
+
+const Testimonials = () => {
+	return (
+		<div className={styles.testimonials}>
+			<div className={styles.details}>
+				<span>TOP RATED</span>
+				<span>
+					SEEDILY SAY HAS SUITABLE DISPOSAL AND BOY. EXERCISE JOY MAN CHILDREN
+					REJOICED
+				</span>
+			</div>
+			<img src={Hero} alt='Hero' />
+
+			<div className={styles.details}>
+				<span>100K</span>
+				<span>HAPPY CUSTOMERS WITH US</span>
+			</div>
+		</div>
+	)
+}
+
+export default Testimonials
